2387d62d6c80e4a118ffb052529cf8418d3f51c3
Lagupro <insfollowers@gmail.com>
2017-04-23 00:16:08 +0700

    Update embedlagupro.js

--- embedlagupro.js
+++ embedlagupro.js
@@ -1,3 +1,3 @@
 <!-- 
-document.writeln(" <link href='https://rawgit.com/Lagupro/embedlagupro/master/0tab-embed.css' rel='stylesheet' type='text/css'/> ");
+document.writeln(" <link href='https://cdn.rawgit.com/Lagupro/embedlagupro/e638ab20/0tab-embed.css' rel='stylesheet' type='text/css'/> ");
  // -->
